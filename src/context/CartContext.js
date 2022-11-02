862c86cdfcb6d158574ac9bea3517a804a64d6b7
damisettimo <damisettimo@gmail.com>
2022-11-02 15:13:07 -0300

    1

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,7 +3,6 @@ import { useState, useEffect, createContext } from "react"
 
 export const CartContext = createContext()
 
-
 export const CartProvider = ({children}) => {
 
   const [cart, setCart] = useState([])
